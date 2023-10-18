c0788b846562a0a944141a5791ef3310d91c9b22
zulvaaraf <zulvasebelas@gmail.com>
2023-10-18 20:12:44 +0700

    inital commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const todos = [];
+const RENDER_EVENT = 'render-todo';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const submitForm = document.getElementById('form');
+  submitForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addTodo();
+  });
+});
+
+function addTodo() {
+  const textTodo = document.getElementById('title').value;
+  const timesTamp = document.getElementById('date').value;
+
+  const generateID = generateId();
+  const todoObject = generateTodoObject(generateID, textTodo, timesTamp, false);
+  todos.push(todoObject);
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function generateId() {
+  return +new Date();
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+  return {
+    id,
+    task,
+    timestamp,
+    isCompleted,
+  };
+}
+
+document.addEventListener(RENDER_EVENT, () => {
+  console.log(todos);
+});
+
+function makeTodo(todoObject) {
+  const textTitle = document.createElement('h2');
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement('p');
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('inner');
+  textContainer.append(textTitle, textTimestamp);
+
+  const container = document.createElement('div');
+  container.classList.add('item', 'shadow');
+  container.append(textContainer);
+  container.setAttribute('id', `todo-${todoObject.id}`);
+
+  return container;
+}
+
+document.addEventListener(RENDER_EVENT, () => {
+  console.log(todos);
+  const uncompletedTODOList = document.getElementById('todos');
+  uncompletedTODOList.innerHTML = '';
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    uncompletedTODOList.append(todoElement);
+  }
+});
